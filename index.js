5eb133a84f60c1b3e338c4034cf6d6cd17dc1a52
Dean Howe <hideanhowe@gmail.com>
2025-11-28 07:50:14 +0000

    steps

--- index.js
+++ index.js
@@ -19,13 +19,21 @@ function compare(word, guess) {
   const secretArray = word.split('');
   const guessArray = guess.split('');
 
+  // run through arrays to compare
   function compareArrays(a, b) {
     let counter = [];
     for (let i = 0; i <= a.length; i++) {
+      // if match push("2")
       if (a[i] === b[i]) {
         counter.push('2');
+      }
+      // if not match check if array contains letter
+      else if (a.includes(b[i])) {
+        counter.push('1');
+        // If a letter occurs multiple times in the guess, it can only match as many times as it appears in the secret word.
       } else {
-        counter.push(b[i]);
+        // if no match and doesnt contain push("0")
+        counter.push('0');
       }
     }
     return counter;
@@ -33,12 +41,12 @@ function compare(word, guess) {
 
   const test = compareArrays(secretArray, guessArray);
   console.log('This is the test string: ', test);
-  // run through arrays to compare
-  // if match push("2")
-  // if not match check if array contains letter
   // if yes push("1")
-  // if no match and doesnt contain push("0")
   // return string
 
   return word;
 }
+
+// The includes() method returns true if an array contains a specified value.
+// The includes() method returns false if the value is not found.
+// The includes() method is case sensitive.
